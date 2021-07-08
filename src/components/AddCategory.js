b793d216a474dc7a2a0603f4206340811f10d88d
Alex <alexxsur@gmail.com>
2021-07-08 12:38:16 -0500

    Simular un submit del formulario

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -7,6 +7,7 @@ export const AddCategory = ({setCategories}) => {
 
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
+        console.log('handleInputChange llamado');
     }
 
     const handleSubmit = (e) => {
@@ -18,7 +19,8 @@ export const AddCategory = ({setCategories}) => {
     }
 
     return (
-            <form onSubmit={handleSubmit}>                
+            <form onSubmit={handleSubmit}>
+                <p>{inputValue}</p>
                 <input
                     type="text"
                     value={ inputValue }
--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -5,10 +5,16 @@ import {AddCategory} from '../../components/AddCategory';
 describe('Pruebas en <AddCategory />', () => {
 
     const setCategories = () => {};
+    const wrapper = shallow(<AddCategory setCategories={setCategories}/>)
 
     test('debe de mostrar el componente correctamente', () => {
-
-        const wrapper = shallow(<AddCategory setCategories={setCategories}/>)
         expect( wrapper ).toMatchSnapshot();
+    })
+
+    test('debe de cambiar la caja de texto', () => {
+        const input = wrapper.find('input');
+        const value =  'Hola Mundo';
+        input.simulate('change', {target: { value } });
+        expect( wrapper.find('p').text().trim()).toBe(value);
     })
 })
