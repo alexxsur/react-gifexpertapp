a8e22a80b5032627ad117fdc13958b4714d3b51d
Alex <alexxsur@gmail.com>
2021-07-08 11:01:07 -0500

    Pruebas del Componente GifGridItem

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,12 +3,23 @@ import {shallow} from 'enzyme';
 import {GifGridItem} from '../../components/GifGridItem';
 
 describe('Pruebas en <GifGridItem />', () => {
+
+    const title = 'Un título';
+    const url = 'http://localhost/algo.jpg';
+    const wrapper = shallow(<GifGridItem title={title} url={url} />);
+
     test('debe de mostrar el componente correctamente', () => {
-
-        const title = 'Un título';
-        const url = 'http://localhost/algo.jpg';
-
-        const wrapper = shallow(<GifGridItem title={title} url={url} />);
         expect( wrapper ).toMatchSnapshot();
+    })
+
+    test('debe de tener un párrafo con el title', () => {
+        const p = wrapper.find('p');
+        expect( p.text().trim() ).toBe(title);
+    })
+
+    test('debe de tener animate__swing', () => {
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+        expect(className.includes('animate__swing')).toBe(true);
     })
 })
